d27bd1ecded642e15855269ea73692e712730148
rrmangum <rrmangum@gmail.com>
2023-04-14 12:18:39 -0700

    Added an additional solution for palindrome problem

--- leetcode-solutions/palindrome.js
+++ leetcode-solutions/palindrome.js
@@ -7,3 +7,26 @@ var isPalindrome = function (x) {
 };
 
 console.log(isPalindrome(121));
+
+// Time complexity: O(1) - Constant
+// Space complexity: O(1) - Constant
+
+// Can also use a deprecating for loop
+
+// var isPalindrome = function (x) {
+//   y = x.toString();
+//   newString = "";
+
+//   for (let i = y.length; i >= 0; i--) {
+//     newString += y[i];
+//   }
+
+//   if (x == y) {
+//     return true;
+//   }
+
+//   return false;
+// };
+
+// Time complexity: O(n) - Linear
+// Space complexity: O(1) - Constant
