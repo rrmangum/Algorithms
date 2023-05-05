7f96e13be0dfbd012f77546beabfae5db1a28c0b
rrmangum <rrmangum@gmail.com>
2023-05-05 13:04:32 -0700

    Searching alogrithm introduction

--- dsa-course/common-patterns/divideAndConquer.js
+++ dsa-course/common-patterns/divideAndConquer.js
@@ -3,4 +3,16 @@
 
 // This pattern can tremendously decrease time complexity (Binary Search is a good example)
 
-//
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+
+    if (target === arr[middle]) return middle;
+    else if (target > arr[middle]) left = middle + 1;
+    else right = middle - 1;
+  }
+  return -1;
+}
---
+++ dsa-course/searching/binarySearch.js
@@ -0,0 +1,47 @@
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+
+    if (target === arr[middle]) {
+      return middle;
+    } else if (target > arr[middle]) {
+      left = middle + 1;
+    } else right = middle - 1;
+  }
+  return -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 2)); // 1
+console.log(binarySearch([1, 2, 3, 4, 5], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5], 5)); // 4
+console.log(binarySearch([1, 2, 3, 4, 5], 6)); // -1
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    10
+  )
+); // 2
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    95
+  )
+); // 16
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    100
+  )
+); // -1
