ce73ca9d603929a0a50dc5d4064f3a5bd03f897b
rrmangum <rrmangum@gmail.com>
2023-05-05 07:46:11 -0700

    Cleaned up the factorial iterative approach

--- math/factorial.js
+++ math/factorial.js
@@ -6,10 +6,10 @@
 // factorial(4) = 4*3*2*1 = 24
 // factorial(5) = 5*4*3*2*1 = 120
 
-const factorial = (n) => {
+const factorial = (num) => {
   let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result = result * i;
+  for (let i = num; i > num; i--) {
+    result += i;
   }
   return result;
 };
